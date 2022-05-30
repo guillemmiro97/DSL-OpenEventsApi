00c378e6b9e284cdbd8b139aebdd689ebb38d537
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-05-30 19:15:38 +0200

    Post messages done

--- DAO/MessagesDAO.js
+++ DAO/MessagesDAO.js
@@ -1,9 +1,40 @@
 const GenericDAO = require("./generic.dao")
 
-class MessagesDAO extends GenericDAO{
+class MessagesDAO extends GenericDAO {
 
-    constructor(){
-        super("user")
+    constructor() {
+        super("message")
+    }
+
+
+    async insertMessage(message) {
+        this._message = message
+
+        /*`id` int(11) NOT NULL,
+        `content` varchar(45) NOT NULL,
+        `user_id_send` int(11) NOT NULL,
+        `user_id_recived` int(11) NOT NULL,
+        `timeStamp` datetime NOT NULL*/
+
+        const [results] = await global.connection.promise()
+            .query("INSERT INTO ?? (content, user_id_send, user_id_recived, timeStamp) VALUES (?, ?, ?, ?)", [this.tabla, this._message.content, this._message.user_id_send, this._message.user_id_recived, new Date()])
+
+
+        if (results.affectedRows > 0) {
+            let lastMessage = await this.getLastMessage()
+            return lastMessage;
+        } else {
+
+        }
+
+    }
+
+    async getLastMessage() {
+        const query = `SELECT * FROM ?? ORDER BY timeStamp DESC LIMIT 1`
+
+        const [results] = await global.connection.promise().query(query, [this.tabla])
+
+        return results
     }
 
 }
--- routes/messages_route.js
+++ routes/messages_route.js
@@ -5,17 +5,24 @@ const MessagesDAO = require("../DAO/MessagesDAO");
 const mdao = new MessagesDAO()
 
 //Creates message
-router.post("/", async (req, res, next) => {
-    res.send("Waiting for implementation")
+router.post("/", async(req, res, next) => {
+
+    if (await mdao.checkToken(req)) {
+        let message = req.body
+        res.status(201).send(await mdao.insertMessage(message))
+    } else {
+        res.sendStatus(401)
+    }
+
 })
 
 //Gets all external users that are messaging the authenticated user
-router.get("/users/", async (req, res, next) => {
+router.get("/users/", async(req, res, next) => {
     res.send("Waiting for implementation")
 })
 
 //Gets all messages between the external user with matching id and the authenticated user
-router.get("/:id/", async (req, res, next) => {
+router.get("/:id/", async(req, res, next) => {
     res.send("Waiting for implementation")
 })
 
