ebe19f3394c8b477ce6ad673ec78d698f1c9b7ed
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-06-02 17:06:01 +0200

    Get of user assistance by id, all future and finished events done

--- DAO/UsersDAO.js
+++ DAO/UsersDAO.js
@@ -14,8 +14,7 @@ class UsersDAO extends GenericDAO {
         this._image = image
 
         const [results] = await global.connection.promise()
-            .query("INSERT INTO ?? (name, last_name, email, password, image) VALUES (?, ?, ?, ?, ?)"
-                , [this.tabla, this._name, this._last_name, this._email, this._password, this._image])
+            .query("INSERT INTO ?? (name, last_name, email, password, image) VALUES (?, ?, ?, ?, ?)", [this.tabla, this._name, this._last_name, this._email, this._password, this._image])
 
         return results;
     }
@@ -40,11 +39,39 @@ class UsersDAO extends GenericDAO {
         this._image = image
 
         const [results] = await global.connection.promise()
-            .query("UPDATE ?? SET name = ?, last_name = ?, email = ?, password = ?, image = ? WHERE id = ?"
-                , [this.tabla, this._name, this._last_name, this._email, this._password, this._image, this._id])
+            .query("UPDATE ?? SET name = ?, last_name = ?, email = ?, password = ?, image = ? WHERE id = ?", [this.tabla, this._name, this._last_name, this._email, this._password, this._image, this._id])
 
         return results;
     }
+
+    async getUserAsistancesById(id, mode) {
+        this._id = id
+        let query = ""
+        switch (mode) {
+            case "future":
+                query = `SELECT * FROM events WHERE id IN ( SELECT event_id FROM assistance WHERE user_id = ?) AND eventStart_date > NOW()`
+                break;
+            case "finished":
+                query = `SELECT * FROM events WHERE id IN ( SELECT event_id FROM assistance WHERE user_id = ?) AND eventStart_date < NOW()`
+                break;
+            case "all":
+                query = `SELECT * FROM events WHERE id IN ( SELECT event_id FROM assistance WHERE user_id = ?)`
+                break;
+            default:
+                break;
+        }
+
+        const [results] = await global.connection.promise().query(query, [this._id])
+
+        if (results.length > 0) {
+            return results
+        } else {
+            return "No assistances found"
+        }
+
+    }
+
+
 
 }
 
--- routes/users_route.js
+++ routes/users_route.js
@@ -8,7 +8,7 @@ const EventsDAO = require("../DAO/EventsDAO");
 const edao = new EventsDAO()
 
 //register
-router.post("/", async (req, res, next) => {
+router.post("/", async(req, res, next) => {
     console.log(req.body)
     await udao.insertUser(req.body.name, req.body.last_name, req.body.email, req.body.password, req.body.image)
     res.status(201).json({
@@ -20,7 +20,7 @@ router.post("/", async (req, res, next) => {
 })
 
 //login
-router.post("/login", async (req, res, next) => {
+router.post("/login", async(req, res, next) => {
     const { email, password } = req.body;
     const users = await udao.getAll();
     const user = users.find(u => u.email === email && u.password === password);
@@ -30,7 +30,7 @@ router.post("/login", async (req, res, next) => {
     if (user) {
         if (user.password === password) {
             const jwt = require('jsonwebtoken');
-            const token = jwt.sign({id: user.id, name: user.name, password: user.password}, process.env.JWT_KEY);
+            const token = jwt.sign({ id: user.id, name: user.name, password: user.password }, process.env.JWT_KEY);
 
             res.status(200).json({
                 accessToken: token
@@ -42,7 +42,7 @@ router.post("/login", async (req, res, next) => {
 })
 
 //get all users
-router.get("/", async (req, res, next)  => {
+router.get("/", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await udao.getAll())
     } else {
@@ -51,7 +51,7 @@ router.get("/", async (req, res, next)  => {
 })
 
 //get users by string
-router.get("/search", async (req, res, next) => {
+router.get("/search", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await udao.getUserByString(req.query.s))
     } else {
@@ -60,7 +60,7 @@ router.get("/search", async (req, res, next) => {
 })
 
 //get user by id
-router.get("/:id", async (req, res, next) => {
+router.get("/:id", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await udao.get(req.params.id))
     } else {
@@ -69,12 +69,12 @@ router.get("/:id", async (req, res, next) => {
 })
 
 //get user statisticts by id
-router.get("/:id/statistics", async (req, res, next) => {
+router.get("/:id/statistics", async(req, res, next) => {
     res.send("Waiting for implementation")
 })
 
 //edit specified fields of the authenticated user
-router.put("/", async (req, res, next) => {
+router.put("/", async(req, res, next) => {
     const decoded = await udao.checkToken(req)
     if (decoded) {
         let user = await udao.get(decoded.id)
@@ -93,7 +93,7 @@ router.put("/", async (req, res, next) => {
 })
 
 //delete authenticated user
-router.delete("/", async (req, res, next) => {
+router.delete("/", async(req, res, next) => {
     const decoded = await udao.checkToken(req)
     if (decoded) {
         res.status(204).send(await udao.delete(decoded.id))
@@ -105,7 +105,7 @@ router.delete("/", async (req, res, next) => {
 //USERS - EVENTS
 
 //get the events created by the user
-router.get("/:id/events", async (req, res, next) => {
+router.get("/:id/events", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await edao.getEventsByUserId(req.params.id, ""))
     } else {
@@ -114,7 +114,7 @@ router.get("/:id/events", async (req, res, next) => {
 })
 
 //get user future events by id
-router.get("/:id/events/future", async (req, res, next) => {
+router.get("/:id/events/future", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await edao.getEventsByUserId(req.params.id, "future"))
     } else {
@@ -123,7 +123,7 @@ router.get("/:id/events/future", async (req, res, next) => {
 })
 
 //get user finished events by id
-router.get("/:id/events/finished", async (req, res, next) => {
+router.get("/:id/events/finished", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await edao.getEventsByUserId(req.params.id, "finished"))
     } else {
@@ -132,7 +132,7 @@ router.get("/:id/events/finished", async (req, res, next) => {
 })
 
 //get user current events by id
-router.get("/:id/events/current", async (req, res, next) => {
+router.get("/:id/events/current", async(req, res, next) => {
     if (await udao.checkToken(req)) {
         res.status(200).send(await edao.getEventsByUserId(req.params.id, "current"))
     } else {
@@ -141,22 +141,34 @@ router.get("/:id/events/current", async (req, res, next) => {
 })
 
 //get user assistance's by id
-router.get("/:id/assistances", async (req, res, next) => {
-    res.send("Waiting for implementation")
+router.get("/:id/assistances", async(req, res, next) => {
+    if (await udao.checkToken(req)) {
+        res.status(200).send(await udao.getUserAsistancesById(req.params.id, "all"))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 //get user future assistance's by id
-router.get("/:id/assistances/future", async (req, res, next) => {
-    res.send("Waiting for implementation")
+router.get("/:id/assistances/future", async(req, res, next) => {
+    if (await udao.checkToken(req)) {
+        res.status(200).send(await udao.getUserAsistancesById(req.params.id, "future"))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 //get user finished assistance's by id
-router.get("/:id/assistances/finished", async (req, res, next) => {
-    res.send("Waiting for implementation")
+router.get("/:id/assistances/finished", async(req, res, next) => {
+    if (await udao.checkToken(req)) {
+        res.status(200).send(await udao.getUserAsistancesById(req.params.id, "finished"))
+    } else {
+        res.sendStatus(401)
+    }
 })
 
 //get user friends by id
-router.get("/:id/friends", async (req, res, next) => {
+router.get("/:id/friends", async(req, res, next) => {
     res.send("Waiting for implementation")
 })
 
